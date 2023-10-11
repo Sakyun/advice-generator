444d8f92795131e47f826ac40e040b3aa9125e72
Loic JOE <joeloic1993@hotmail.com>
2023-10-11 14:20:42 +0200

    Added generic layout and styling of the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import iconDice from './assets/icon-dice.svg';
 import './App.css'
 
+type advice = {
+    id?: number;
+    advice?: string;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [advice, setAdvice] = useState<advice>({});
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg">
+          <div className="card">
+              <div className="card-title">Advice #117</div>
+              <div className="card-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, ex!</div>
+              <div className="card-separator"></div>
+              <button className="card-btn">
+                  <img src={iconDice} alt=""/>
+              </button>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
